774236d53390138cface4051c7fbfc58e96a9d58
Mohamed Meftah <mohamed-ted@live.com>
2021-04-02 20:13:22 +0100

    fix module not found with imagetools

--- src/images.d.ts
+++
@@ -1,2 +0,0 @@
-declare module '@/assets/logo.png?w=300;400;500&format=webp&srcset'
-declare module '*.jpeg'
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,5 @@
-<!-- <script ✂prettier:content✂="CiAgaW1wb3J0IFByb2plY3RzIGZyb20gJy4vX3BhcnRpYWxzL3Byb2plY3RzLnN2ZWx0ZScKICBpbXBvcnQgV3JpdGluZ3MgZnJvbSAnLi9fcGFydGlhbHMvd3JpdGluZ3Muc3ZlbHRlJwo=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=">{}</script>
-
-<style ✂prettier:content✂="CiAgLmZpbGxlciB7CiAgICBmb250LXNpemU6IDJyZW07CiAgICBtYXgtd2lkdGg6IHZhcigtLW1heC13aWR0aCk7CiAgICBtYXJnaW46IDAgYXV0bzsKICAgIG1hcmdpbi1ib3R0b206IDJyZW07CiAgfQo=" ✂prettier:content✂="" ✂prettier:content✂="" ✂prettier:content✂="" ✂prettier:content✂="" ✂prettier:content✂=""></style>
-
-<h1>Mohamed Ted Meftah</h1>
-<p>
-  I'm a passionate & entrepreneurial JavaScript developer from the Algeria who
-  likes to push the web beyond it's limits. I specialize in modern frontend
-  using React & Svelte and I also feel at home on the backend using
-  Node/TypeScript.
-</p>
-<p>
-  I'm interested in helping inspiring companies create innovative web apps that
-  users love.
-</p>
-
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<Projects />
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<p class="filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nam reiciendis incidunt quisquam ducimus consequatur, culpa ullam temporibus, similique maiores enim eum ipsa recusandae minima ad mollitia sed, numquam fugit?</p>
-<Writings /> -->
-<script>
-	import Logo from '@/assets/logo.png?w=300;400;500&format=webp&srcset'
+<script lang="ts">
+	const Logo = require('@/assets/logo.png?w=300;400;500&format=webp&srcset')
 	function redirect(e) {
 		e.preventDefault()
 		window.location.href = '/learning'
