3618480cafedeb1374fa7d64b2da2c906246adb2
Mohamed Meftah <mohamed-ted@live.com>
2021-04-11 18:54:55 +0100

    test: render env vars

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,11 +4,13 @@
 		e.preventDefault()
 		window.location.href = '/learning'
 	}
+	console.log(process.env['RENDER_EXTERNAL_URL'])
 </script>
 <Home />
 
 <a on:click={redirect} href="/">Learning in Public</a>
 
+{process.env['RENDER_EXTERNAL_URL']}
 <style lang="postcss">
 </style>
 
