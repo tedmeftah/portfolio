88258e36b33c7ba353929eba8e61dbcc135bb695
Mohamed Meftah <mohamed-ted@live.com>
2021-04-13 20:25:13 +0100

    upgrade packages

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,7 +1,7 @@
 const path = require('path')
 const preprocess = require('svelte-preprocess')
 const adapter = require('@sveltejs/adapter-static')
-const {imagetools} = require('vite-imagetools')
+const { imagetools } = require('vite-imagetools')
 const markdown = require('vite-plugin-svelte-md').default
 const { preprocess: windicss } = require('svelte-windicss-preprocess')
 const { dependencies } = require('./package.json')
@@ -34,4 +34,3 @@ module.exports = {
 		}
 	}
 }
-
