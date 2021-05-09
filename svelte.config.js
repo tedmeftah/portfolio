87482974b99ec2594a6014e214791957f313c825
Mohamed Meftah <mohamed-ted@live.com>
2021-05-09 09:00:45 +0100

    fix missing styles in build

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import path from 'path'
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
@@ -6,16 +7,11 @@ import markdown from '@netulip/rollup-plugin-md'
 import svg from '@netulip/rollup-plugin-svg'
 import windicss from 'vite-plugin-windicss'
 
-// import { dependencies } from './package.json'
+const { dependencies } = JSON.parse(fs.readFileSync('./package.json'))
 
 export default {
 	extensions: ['.svelte', '.md', '.svg'],
-	preprocess: [
-		preprocess({
-			preserve: ['ld+json'],
-			postcss: true
-		})
-	],
+	preprocess: [preprocess({ preserve: ['ld+json'] })],
 	kit: {
 		adapter: adapter(),
 
@@ -26,12 +22,12 @@ export default {
 				}
 			},
 			ssr: {
-				// noExternal: Object.keys(dependencies || {})
+				noExternal: Object.keys(dependencies || {})
 			},
 			plugins: [
 				svg.default({ enforce: 'pre' }),
 				markdown.default({ enforce: 'pre' }),
-				windicss.default(),
+				windicss.default({ transformCSS: 'pre' }),
 				imagetools({ force: true })
 			]
 		}
