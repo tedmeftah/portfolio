702e952250c7cd1635d4cd5bbea5ec98d9e6c9ba
Mohamed Meftah <mohamed.ted.meftah@gmail.com>
2021-12-12 22:07:11 +0100

    test pagination

---
+++ src/routes/articles/pages/[page].svelte
@@ -0,0 +1,13 @@
+<script context="module">
+	export async function load({ page }) {
+		return {
+			props: { page: page.params.page }
+		}
+	}
+</script>
+
+<script>
+	export let page
+</script>
+
+{page}
